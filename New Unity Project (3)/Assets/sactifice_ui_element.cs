e234715e0c0056d223853590d8e77da61358291a
Joel Sjöberg <joelsjoberg3@gmail.com>
2018-12-02 14:28:19 +0200

    Assets added
    prepare for final version by polishing, still need to implement sound and christmas effects

---
+++ New Unity Project (3)/Assets/sactifice_ui_element.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class sactifice_ui_element : MonoBehaviour {
+
+    [SerializeField]Image sacrifice_prompt;
+    [SerializeField] Image launch_prompt;
+    state_script state;
+	// Use this for initialization
+	void Start () {
+        state = GetComponent<state_script>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (state.getDead() || !state.getLaunched())
+        {
+            sacrifice_prompt.gameObject.SetActive(false);
+        }
+        if (state.getLaunched() && !state.getDead())
+        {
+            launch_prompt.gameObject.SetActive(false);
+            sacrifice_prompt.gameObject.SetActive(true);
+        }
+        if (!state.getDead() && !state.getLaunched())
+        {
+            launch_prompt.gameObject.SetActive(true);
+        }
+
+    }
+}
